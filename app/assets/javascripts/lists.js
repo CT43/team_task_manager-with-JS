16dda966b59a601be7bd557559d86247047a036a
github username <github email address>
2018-10-18 15:04:30 -0600

    fix index resource

--- app/assets/javascripts/lists.js
+++ app/assets/javascripts/lists.js
@@ -80,36 +80,16 @@ List.prototype.renderDiv = function(){
 }
 
 $(document).on('turbolinks:load', function(){
+  $("a#lists_link").on("click", function(e){
+    $.get(this.href).success(function(response){
+      var $ol = $("div#listed_lists ol")
+      $ol.html("")
+      response.forEach(function(list){
+        $ol.append("<li><a href="+`/users/${list.user_id}/lists/${list.id}`+">"+list.name+"</a></li>")
+      })
+    })
+
+    e.preventDefault()
+  })
   List.ready()
 })
-
-
-// $(document).on('turbolinks:load', function(){
-//   $("a#lists_link").on("click", function(e){
-//     $.get(this.href).success(function(response){
-//       var $ol = $("div#listed_lists ol")
-//       $ol.html("")
-//       debugger
-//       response.forEach(function(list){
-//         $ol.append("<li><a href="+`/users/${list.user_id}/lists/${list.id}`+">"+list.name+"</a></li>")
-//       })
-//     })
-//
-//     e.preventDefault()
-//   })
-//
-//   $("a.list_nav_link").on("click", function(e){
-//     $.get(this.href).success(function(response){
-//       var $lists = $("div#lists")
-//       $lists.html("")
-//       debugger
-//
-//
-//       var html = '<% escape_javascript @list%>'
-//       $lists.html(html)
-//     })
-//
-//
-//     e.preventDefault()
-//   })
-// })
